5af3f08c6c018001681bd9994f452a292279c205
jadebernardes <jadebernardess@gmail.com>
2020-09-10 11:45:29 -0300

    criando a cobrinha

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ let canvas= document.getElementById("snake");
 //renderiza o desenho dentro do canvas
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
 //funcao que desenha e define a cor do BG
 //fillStyle define o estilo: cor etc
@@ -11,4 +16,12 @@ function criarBG(){
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+function criarCobrinha(){
+    for(i=0; i<snake.length; i++){
+        context.fillStyle = "green";
+        context.fillRect (snake[i].x, snake[i].y, box, box);
+    }
+}
+
 criarBG();
+criarCobrinha();
