e0a24ee6a64ba62529ae477ff8d97f79d99bab9b
jadebernardes <jadebernardess@gmail.com>
2020-09-10 12:02:24 -0300

    definindo o movimento da cobrinha

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
 
 //funcao que desenha e define a cor do BG
 //fillStyle define o estilo: cor etc
@@ -23,5 +24,26 @@ function criarCobrinha(){
     }
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
+    // criar a posição da cobrinha para o ponto de partida
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+    //criar coordenadas da cobrinha por onde ela vai seguir
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100);
